5172d72ed47b35fd56ac9897f59c5f03d6ffeb04
Sid <sidchopp@gmail.com>
2021-12-10 15:22:46 -0700

    Started with an MUI template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Trial from './components/Trial';
 
 function App() {
   return (
-    <div className="App">
-      Hi
+    <div >
+      <Trial />
     </div>
   );
 }
