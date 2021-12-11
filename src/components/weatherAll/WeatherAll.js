ff6a8ec937fb5db3e3e04d0b8aafa9b9ebb50c29
Sid <sidchopp@gmail.com>
2021-12-10 17:57:11 -0700

    WeatherToday comp created and data fetched from API

--- src/components/weatherAll/WeatherAll.js
+++ src/components/weatherAll/WeatherAll.js
@@ -1,12 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import WeatherForecast from '../weatherForecast/WeatherForecast'
 import WeatherToday from '../weatherToday/WeatherToday'
+import Loader from '../loader/Loader'
 
+
+import { formatISO, startOfYesterday } from 'date-fns'
+
+// Date in ISO format For Yesterday's weather
+const yesterday = startOfYesterday()
+const yesterdayIso = formatISO(yesterday, { representation: 'date' })
+// console.log(yesterdayIso);
 function WeatherAll() {
+
+  //States
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+  const [dataYesterday, setDataYesterday] = useState({});
+
+  // Promisifying the Geolocation API
+  const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
+
+  const whereAmI = async function () {
+    setLoading(true);
+    // Geo Location
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lon } = pos.coords;
+    // console.log(pos.coords);
+
+    try {
+
+      // For today's weather and for weather forecast
+      const responseGeo = await fetch(`${process.env.REACT_APP_API_URL}/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${lat},${lon}&days=7&aqi=yes&alerts=yes`)
+      // console.log("Response is:", responseGeo);
+      const dataGeo = await responseGeo.json();
+      // console.log(dataGeo);
+      setLoading(false)
+      setData(dataGeo)
+
+      // For yesterday's weather
+      const responseYesterday = await fetch(`${process.env.REACT_APP_API_URL}/history.json?key=${process.env.REACT_APP_API_KEY}&q=${lat},${lon}&dt=${yesterdayIso}`)
+      const dataYesterday = await responseYesterday.json();
+      // console.log(dataYesterday);
+      setDataYesterday(dataYesterday);
+
+    } catch (err) {
+      setLoading(false)
+      console.log('This is the error:', err.message);
+    }
+
+
+  }
+  useEffect(() => {
+    whereAmI();
+  }, [])
+
+  // if data is not fetched,show loader
+  if (loading) {
+    return (
+      <Loader />
+    )
+  }
+
   return (
     <>
-      <WeatherToday />
-      <WeatherForecast />
+      {(typeof data.current !== 'undefined' && typeof data.location !== 'undefined' && typeof data.forecast !== 'undefined' && typeof dataYesterday.forecast !== 'undefined')
+        ? <>
+          <WeatherToday data={data} />
+
+          <WeatherForecast data={data} />
+
+        </>
+        : <div></div>
+      }
     </>
 
   )
--- src/components/weatherToday/WeatherToday.js
+++ src/components/weatherToday/WeatherToday.js
@@ -1,38 +1,70 @@
 import * as React from 'react';
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
-// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import CardHeader from '@mui/material/CardHeader';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Grid from '@mui/material/Grid';
-// import StarIcon from '@mui/icons-material/StarBorder';
-// import Toolbar from '@mui/material/Toolbar';
+
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-// import Link from '@mui/material/Link';
-// import GlobalStyles from '@mui/material/GlobalStyles';
+import ButtonBase from '@mui/material/ButtonBase';
 import Container from '@mui/material/Container';
-// import { ReplayCircleFilledTwoTone } from '@mui/icons-material';
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+});
 
 
-
-function WeatherToday() {
+function WeatherToday({ data }) {
+  console.log(data);
   return (
     <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
-      <Typography
-        component="h1"
-        variant="h2"
-        align="center"
-        color="text.primary"
-        gutterBottom
-      >
-        Weather Today
-      </Typography>
-      <Typography variant="h5" align="center" color="text.secondary" component="p">
-        0 ° C
-      </Typography>
+      <Paper sx={{ p: 2, margin: 'auto', maxWidth: 500, flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item>
+
+            <Typography variant="h7" align="center" color="text.secondary" component="p">
+              Feels Like
+            </Typography>
+            <Typography variant="h5" align="center" color="text.primary" component="p">
+              {data.current.feelslike_c}°
+            </Typography>
+
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography variant="h1" gutterBottom align="center" color="text.secondary" component="p">
+                  {data.current.temp_c}°
+                </Typography>
+
+                <Typography
+                  component="h1"
+                  variant="h4"
+                  align="center"
+                  color="text.primary"
+                >
+                  {data.location.name}
+                </Typography>
+                <Typography variant="h7" align="center" color="text.secondary" component="p">
+                  {data.location.region}, {data.location.country}
+                </Typography>
+              </Grid>
+              {/* <Grid item>
+                <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                  Remove
+                </Typography>
+              </Grid> */}
+            </Grid>
+            <Grid item>
+              <Typography variant="h6" align="center" color="text.primary" component="p">
+                {data.current.condition.text}
+                <Img src={data.current.condition.icon} alt="Weather" />
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
     </Container>
   )
 }
