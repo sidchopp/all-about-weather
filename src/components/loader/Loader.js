b0bdb2b713c23a5543fb75c6d5a1a7930e7967e4
Sid <sidchopp@gmail.com>
2021-12-29 13:26:07 -0700

    Loader at middle

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+
+//CSS
+import '../../App.css'
 
 export default function CircularIndeterminate() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
-    </Box>
+    <div className='App'>
+      <CircularProgress >Loading..</CircularProgress>
+    </div>
   );
 }
