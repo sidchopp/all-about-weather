9156c372e178afd85d76b32fb122420043e461e7
Sid <sidchopp@gmail.com>
2022-02-07 11:02:39 -0700

    Router and navbar removed

--- src/App.js
+++ src/App.js
@@ -3,34 +3,18 @@ import './App.css';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import CssBaseline from '@mui/material/CssBaseline';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
-
 //Components
 import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
 import WeatherAll from './components/weatherAll/WeatherAll';
-import WeatherForecast from './components/weatherForecast/WeatherForecast';
 
 function App() {
   return (
-    <Router>
-      <React.Fragment>
-        <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-        <CssBaseline />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<WeatherAll />} />
-          <Route path="/forecast" element={<WeatherForecast />} />
-        </Routes>
-        <Footer />
-      </React.Fragment>
-    </Router>
+    <React.Fragment>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      <WeatherAll />
+      <Footer />
+    </React.Fragment>
   );
 }
 
