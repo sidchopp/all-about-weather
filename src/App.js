2827ad7c57fb7e16cdaabd7d2902a3cb392a2172
Sid <sidchopp@gmail.com>
2022-03-25 14:53:17 -0600

    colors changed to shades of blue and MUI styles removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import CssBaseline from '@mui/material/CssBaseline';
 
 //Components
 import Footer from './components/footer/Footer';
@@ -10,8 +8,6 @@ import WeatherAll from './components/weatherAll/WeatherAll';
 function App() {
   return (
     <React.Fragment>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-      <CssBaseline />
       <WeatherAll />
       <Footer />
     </React.Fragment>
