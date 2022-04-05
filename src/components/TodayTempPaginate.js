3134be0263e138456ac3b4bc732e621a5fe0d47c
Sid <sidchopp@gmail.com>
2022-04-05 15:47:33 -0600

    button functionality included to change temp slides

--- src/components/TodayTempPaginate.js
+++ src/components/TodayTempPaginate.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import paginate from "./paginate";
 import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 
 const Img = styled('img')({
   margin: 'auto',
@@ -14,36 +15,53 @@ const TodayTempPaginate = ({ data }) => {
   const [today] = forecastday;
   const { hour } = today;
   // console.log(hour);
-  paginate(hour);
+  // paginate(hour);
   //state
   const [slide, setSlide] = useState(0);
+  const [totalSlides, setTotalSlides] = useState(paginate(hour))
   const [temperatures, setTemperatures] = useState([]);
-  console.log(slide);
+  // console.log(totalSlides);
 
   useEffect(() => {
     setTemperatures(paginate(hour)[slide])
-  }, [])
-  console.log(slide);
+  }, [slide, hour]);
+
+  const handleSlides = (index) => {
+    setSlide(index)
+  }
 
   return (
-    <div className='paginate-rows'>
-      {temperatures.map(value => {
-        return (
-          <div key={value.time_epoch} className='temp-paginate-card' >
-            <div>
-              <Img src={value.condition.icon} />
-            </div>
-            <div >
-              {value.time.slice(11, 16)}
+    <>
+      <div className='paginate-rows'>
+        {temperatures.map(value => {
+          return (
+            <div key={value.time_epoch} className='temp-paginate-card' >
               <div>
-                {value.temp_c} °C
+                <Img src={value.condition.icon} />
+              </div>
+              <div style={{ textAlign: 'center', fontSize: '0.7rem' }} >
+                {value.time.slice(11, 16)}
+                <div style={{ fontSize: "1rem" }}  >
+                  {Math.round(value.temp_c)}
+                  <span style={{ textAlign: 'center', fontSize: '0.5rem' }}>°C</span>
+                </div>
               </div>
             </div>
-          </div>
-        )
-      }
-      )}
-    </div>
+          )
+        }
+        )}
+      </div>
+      <div style={{ margin: '5px', textAlign: 'center' }}>
+        {totalSlides.map((item, index) => {
+          return (
+            <button
+              key={index}
+              className={` page-btn ${index === slide ? "active-btn" : null}`}
+              onClick={() => handleSlides(index)}
+            >{index + 1}</button>)
+        })}
+      </div>
+    </>
   )
 }
 
