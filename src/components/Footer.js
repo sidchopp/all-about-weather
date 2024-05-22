b510a4cd5e04e7261a4f8c32e986c7cbb6c7990c
sidchopp <sidchopp@gmail.com>
2024-05-22 13:59:02 -0600

    Updated my new website name in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
 
 function Footer() {
   return (
-    <div className='footer' >
+    <div className="footer">
       <Container maxWidth="md" component="footer">
         <Typography
           variant="subtitle1"
@@ -12,22 +12,22 @@ function Footer() {
           component="p"
           gutterBottom
         >
-          <span className='font'>
-            ALL ABOUT WEATHER
-          </span>
+          <span className="font">ALL ABOUT WEATHER</span>
         </Typography>
-        <Typography variant="body2" color="text.secondary" align="center" >
-          <span className='font'>   {'Copyright © '}
+        <Typography variant="body2" color="text.secondary" align="center">
+          <span className="font">
+            {" "}
+            {"Copyright © "}
             {new Date().getFullYear()}
-            {'.'} {' '}
-            <Link href="https://sid-projects.netlify.app/" target="_blank">
+            {"."}{" "}
+            <Link href="https://sid-chopra.netlify.app/" target="_blank">
               My Other Projects
             </Link>
           </span>
         </Typography>
       </Container>
     </div>
-  )
+  );
 }
 
 export default Footer;
